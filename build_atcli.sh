df94690603738b83b4d799d5ef5fc8577280942d
Colin Constable <colin@constable.cc>
2021-09-02 22:21:48 -0700

    Tidier
    moved some files around to keep things clearer and to get a Dockerfile in top directory

--- build_atcli.sh
+++ build_atcli.sh
@@ -2,4 +2,4 @@
 # docker version needs be greater than 19.0.3.0
 # user docker --version to check the installed version
 export DOCKER_BUILDKIT=1
-docker build --file Dockerfile --output ./ .
+docker build --file at_cli/Dockerfile --output ./ .
